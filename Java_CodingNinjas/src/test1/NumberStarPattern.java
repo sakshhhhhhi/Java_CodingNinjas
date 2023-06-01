7376adcc203f9011a76e20814c0602d1fe665c79
sakshhhhhhi <sakshisubir@gmail.com>
2023-06-01 20:56:56 +0530

    Test 1

---
+++ Java_CodingNinjas/src/test1/NumberStarPattern.java
@@ -0,0 +1,24 @@
+package test1;
+
+import java.util.Scanner;
+
+public class NumberStarPattern {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i=1;i<=n;i++) {
+			for(int j=n;j>0;j--) {
+				if(j==i) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(j);
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
