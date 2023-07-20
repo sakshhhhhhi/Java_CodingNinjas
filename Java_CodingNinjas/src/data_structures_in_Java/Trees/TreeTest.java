6c5158a8cc8dd503493b21e7d809e07e2d405b7a
sakshhhhhhi <sakshisubir@gmail.com>
2023-07-20 14:21:45 +0530

    Trees

--- Java_CodingNinjas/src/data_structures_in_Java/Trees/TreeTest.java
+++ Java_CodingNinjas/src/data_structures_in_Java/Trees/TreeTest.java
@@ -1,5 +1,36 @@
 package data_structures_in_Java.Trees;
+
+import java.util.*;
+
 public class TreeTest {
+	
+	public static TreeNode<Integer> TakeInput(){
+		
+		Scanner sc = new Scanner(System.in);
+		
+		Queue<TreeNode<Integer>> nodesToprint = new LinkedList<>();
+		System.out.println("Enter the root");
+		int rootData=sc.nextInt();
+		if(rootData==-1) {
+			return null;
+		}
+		TreeNode<Integer> root = new TreeNode<Integer>(rootData);
+		
+		nodesToprint.add(root);
+		while(!nodesToprint.isEmpty()) {
+			TreeNode<Integer> front = nodesToprint.remove();
+			System.out.println("Enter no of children of :" + front.data);
+		    int numOfChildren = sc.nextInt();
+		    for(int i =0;i<numOfChildren;i++) {
+		    	System.out.println("Enter the "+i+"th child");
+		    	int ChildData =sc.nextInt();
+		    	TreeNode<Integer> childNode = new TreeNode<Integer>(ChildData);
+		    	front.children.add(childNode);
+		    	nodesToprint.add(childNode);
+		    }
+		}
+		return root;
+	}
 	public static void printTree(TreeNode<Integer> root) {
 		if(root==null) {
 			return;
@@ -27,18 +58,19 @@ public class TreeTest {
 	}
 
 	public static void main(String[] args) {
-		TreeNode<Integer> root = new TreeNode<>(4);
-		TreeNode<Integer> node1 = new TreeNode<>(2);
-		TreeNode<Integer> node2 = new TreeNode<>(3);
-		TreeNode<Integer> node3 = new TreeNode<>(1);
-		TreeNode<Integer> node4 = new TreeNode<>(5);
-		TreeNode<Integer> node5 = new TreeNode<>(6);
-		root.children.add(node1);
-		root.children.add(node2);
-		root.children.add(node3);
-		
-		node2.children.add(node4);
-		node2.children.add(node5);
+//		TreeNode<Integer> root = new TreeNode<>(4);
+//		TreeNode<Integer> node1 = new TreeNode<>(2);
+//		TreeNode<Integer> node2 = new TreeNode<>(3);
+//		TreeNode<Integer> node3 = new TreeNode<>(1);
+//		TreeNode<Integer> node4 = new TreeNode<>(5);
+//		TreeNode<Integer> node5 = new TreeNode<>(6);
+//		root.children.add(node1);
+//		root.children.add(node2);
+//		root.children.add(node3);
+//		
+//		node2.children.add(node4);
+//		node2.children.add(node5);
+		TreeNode<Integer> root = TakeInput(); 
 		printTreeDetailed(root);
 
 	}
