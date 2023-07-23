47001ddd517b7bc6676645a717cd6600ecb392db
sakshhhhhhi <sakshisubir@gmail.com>
2023-07-24 01:14:38 +0530

    HashMaps

---
+++ Java_CodingNinjas/src/data_structures_in_Java/Hashmaps/PrintIntersection.java
@@ -0,0 +1,39 @@
+package data_structures_in_Java.Hashmaps;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class PrintIntersection {
+	
+	public static void main(String[] args) {
+		int [] arr1 = {2,3,4,5};
+		int [] arr2 = {1,2,6,4};
+		printIntersection(arr1,arr2);
+	}
+
+	public static void printIntersection(int[] arr1, int[] arr2) {
+		// TODO Auto-generated method stub
+		HashMap<Integer,Integer> map = new HashMap<>();
+		for(int key:arr1) {
+			if(map.containsKey(key)) {
+				map.put(key, map.get(key)+1);
+			}
+			else {
+				map.put(key, 1);
+			}
+		}
+		Arrays.sort(arr2);
+		for(int key:arr2) {
+			if(map.containsKey(key)) {
+				int val =map.get(key);
+				if(val>0) {
+					System.out.println(key);
+					map.put(key, val-1);
+				}
+			}
+		}
+		
+	}
+
+
+}
